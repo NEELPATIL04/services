fabf0a9076b7e593f3e6d7ba8db34788269bd204
NEELPATIL04 <neel1234patil1@gmail.com>
2025-03-12 23:49:21 +0530

    ok

--- project/app/layout.tsx
+++ project/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="canonical" href="https://yourwebsite.com" />
+        <link rel="canonical" href="https://webdrift.com" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
